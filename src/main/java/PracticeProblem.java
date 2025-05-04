595897bda0b4200bac3872fe12211d425f8230db
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-05-04 16:28:46 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		int[] cost = {10, 15};
+		System.out.println("Final: " + minCostClimbingStairs (cost));
 
 	}
 	
@@ -27,6 +29,68 @@ public class PracticeProblem {
 		}
 		values.put(num, fibHelper (num - 1, values) + fibHelper (num - 2, values));
 		return values.get(num);
+
+	}
+
+
+
+	public static int minCostClimbingStairs(int[] cost)
+	{
+		System.out.println("Runs");
+
+		HashMap<Integer, Integer> leastCostAtIndex = new HashMap<Integer, Integer>(); 
+		if (cost.length < 1)
+		{
+			return 0;
+		}
+		else if(cost.length == 1)
+		{
+			return cost[0];
+		}
+		return minCostHelper(-1, cost, cost.length, leastCostAtIndex);
+
+
+	}
+
+
+	public static int minCostHelper (int currentIndex, int[] cost, int costLength, HashMap<Integer, Integer> leastCostAtIndex)
+	{
+		if (currentIndex >= costLength)
+		{
+			System.out.println("Got to 0");
+			return 0; 
+		}
+		System.out.println(currentIndex);
+
+		if (leastCostAtIndex.get(currentIndex)!= null)
+		{
+			return leastCostAtIndex.get(currentIndex);
+		}
+		else
+		{
+			int oneStep = minCostHelper(currentIndex + 1, cost, costLength, leastCostAtIndex);
+			int twoStep = minCostHelper(currentIndex + 2, cost, costLength, leastCostAtIndex);
+			if (currentIndex >= 0)
+			{
+				if (oneStep < twoStep)
+				{
+					leastCostAtIndex.put(currentIndex, oneStep + cost[currentIndex]);
+					return oneStep + cost[currentIndex];
+				}
+				leastCostAtIndex.put(currentIndex, twoStep + cost[currentIndex]);
+				return twoStep + cost[currentIndex];
+			}
+			
+			if (oneStep < twoStep)
+			{
+				leastCostAtIndex.put(currentIndex, oneStep );
+				return oneStep;
+			}
+			leastCostAtIndex.put(currentIndex, twoStep);
+			return twoStep;
+		}
+
+
 
 	}
 
